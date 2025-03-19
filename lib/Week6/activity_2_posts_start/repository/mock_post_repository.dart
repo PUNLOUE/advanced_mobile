7c522d1b14006aea64718b9eeaf8c7209cc6f0b9
PUNLOUE <160609048+PUNLOUE@users.noreply.github.com>
2025-03-20 03:13:20 +0700

    Start code

---
+++ lib/Week6/activity_2_posts_start/repository/mock_post_repository.dart
@@ -0,0 +1,12 @@
+import '/Week6/activity_2_posts_start/model/post.dart';
+import '/Week6/activity_2_posts_start/repository/post_repository.dart';
+// TODO
+
+class MockPostRepository extends PostRepository {
+  @override
+  Future<Post> getPost(int postId) {
+    // TODO: implement getPost
+    throw UnimplementedError();
+  }
+}
+ 
